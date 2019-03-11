1f41982a9f8ed27b6322b337869f9154568c7025
Evan Gray <egray1985@gmail.com>
2019-03-11 18:07:24 -0500

    prefer guard clauses

--- cdc.plugin.bash
+++ cdc.plugin.bash
@@ -7,15 +7,14 @@ fi
 
 cdc() {
 
-    [[ -z $REPO_DIRS && -f $HOME/.cdcrc ]] && source $HOME/.cdcrc
+    [[ -z $REPO_DIRS && -f $HOME/.cdcrc ]] && source "$HOME/.cdcrc"
 
     local dir
+    local wdir
     local cd_dir="${1%%/*}"
     local USAGE="cdc: [DIRECTORY]"
 
-    if [[ "$1" == */* ]]; then
-        local subdir="${1#*/}"
-    fi
+    [[ "$1" == */* ]] && local subdir="${1#*/}"
 
     if (( ${#CDC_DIRS[@]} == 0 )); then
         echo "You must either \`export CDC_DIRS=()\` as an environmental" >&2
@@ -27,21 +26,21 @@ cdc() {
     fi
 
     for dir in ${CDC_DIRS[@]}; do
-        if [[ -d $dir ]]; then
-            if [[ -d $dir/$cd_dir ]]; then
-                cd "$dir/$cd_dir"
-                if [[ -n $subdir ]]; then
-                    if [[ -d $subdir ]]; then
-                        cd "$subdir"
-                    else
-                        echo "[$subdir] does not exist in [$cd_dir]." >&2
-                    fi
-                fi
-                return 0
-            fi
-        else
+        if ! [[ -d $dir ]]; then
             echo "[$dir] is exported in \$CDC_DIRS but is not a directory" >&2
+            continue
         fi
+        [[ -d $dir/$cd_dir ]] || continue
+        wdir="$dir/$cd_dir"
+        if [[ -n $subdir ]]; then
+            if [[ -d $wdir/$subdir ]]; then
+                wdir+="/$subdir"
+            else
+                echo "[$subdir] does not exist in [$cd_dir]." >&2
+            fi
+        fi
+        cd "$wdir"
+        return 0
     done
 
     echo "[$cd_dir] not found in ${CDC_DIRS[@]}" >&2
